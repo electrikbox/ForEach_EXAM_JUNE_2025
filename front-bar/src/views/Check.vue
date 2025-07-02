c4f9e4236cb47ab99e5f01715a899651cc1a0a07
electrikbox <electrikbox@icloud.com>
2025-07-02 19:39:37 +0200

    clean

--- front-bar/src/views/Check.vue
+++ front-bar/src/views/Check.vue
@@ -4,5 +4,7 @@
 
 
 <template>
-<h1 class="flex flex-col items-center justify-center gap-4 min-h-screen">Check</h1>
+  <div>
+    <h1 class="flex flex-col items-center justify-center gap-4 min-h-screen">Check</h1>
+  </div>
 </template>
