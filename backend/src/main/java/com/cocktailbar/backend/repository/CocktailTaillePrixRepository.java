da5d0612c6cf39a71bf5dacb3d5b8c5f1117f70d
electrikbox <electrikbox@icloud.com>
2025-07-04 13:28:58 +0200

    update repositories

---
+++ backend/src/main/java/com/cocktailbar/backend/repository/CocktailTaillePrixRepository.java
@@ -0,0 +1,11 @@
+package com.cocktailbar.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cocktailbar.backend.model.CocktailTaillePrix;
+import com.cocktailbar.backend.model.CocktailTaillePrixId;
+
+@Repository
+public interface CocktailTaillePrixRepository extends JpaRepository<CocktailTaillePrix, CocktailTaillePrixId> {
+} 
--- backend/src/main/java/com/cocktailbar/backend/repository/UtilisateurRepository.java
+++ backend/src/main/java/com/cocktailbar/backend/repository/UtilisateurRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cocktailbar.backend.model.Utilisateur;
 
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer> {
-    // Pas besoin d'ajouter de méthodes ici pour les opérations CRUD de base.
-    // JpaRepository fournit déjà tout (save, findById, findAll, deleteById, etc.).
     Optional<Utilisateur> findByEmailUtilisateur(String email);
 }
